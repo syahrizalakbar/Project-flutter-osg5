11b0d82894169532501e837af087ae5ed1d5ba3b
Iman Nurohman <imannurohman1@gmail.com>
2019-09-30 21:16:15 +0700

    DataModel

---
+++ lib/DataModel.dart
@@ -0,0 +1,46 @@
+
+import 'dart:convert';
+
+RootResponse rootResponseFromJson(String str) => RootResponse.fromJson(json.decode(str));
+
+String rootResponseToJson(RootResponse data) => json.encode(data.toJson());
+
+class RootResponse {
+  List<Meal> meals;
+
+  RootResponse({
+    this.meals,
+  });
+
+  factory RootResponse.fromJson(Map<String, dynamic> json) => RootResponse(
+    meals: List<Meal>.from(json["meals"].map((x) => Meal.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "meals": List<dynamic>.from(meals.map((x) => x.toJson())),
+  };
+}
+
+class Meal {
+  String strMeal;
+  String strMealThumb;
+  String idMeal;
+
+  Meal({
+    this.strMeal,
+    this.strMealThumb,
+    this.idMeal,
+  });
+
+  factory Meal.fromJson(Map<String, dynamic> json) => Meal(
+    strMeal: json["strMeal"],
+    strMealThumb: json["strMealThumb"],
+    idMeal: json["idMeal"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "strMeal": strMeal,
+    "strMealThumb": strMealThumb,
+    "idMeal": idMeal,
+  };
+}
